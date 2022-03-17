38139865dfd141da8843678736c0e1acd469c292
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2022-03-17 19:52:11 -0300

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Anderson Sprenger on 17/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
